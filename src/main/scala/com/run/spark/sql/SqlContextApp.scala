d9d23630c47bf30816e7f3e3adbf005dc01b8e7d
yanglin89 <yanglin9855@163.com>
2019-05-11 00:29:28 +0800

    sparksql

---
+++ src/main/scala/com/run/spark/sql/SqlContextApp.scala
@@ -0,0 +1,20 @@
+package com.run.spark.sql
+
+/**
+  * spqrk sqlContext 的使用
+  * */
+object SqlContextApp {
+
+  def main(args: Array[String]): Unit = {
+
+    // 步骤共有三步
+    //1、创建相应的context
+
+    //2、业务逻辑处理
+
+    //3、释放资源
+
+  }
+
+
+}
